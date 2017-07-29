4fc8c4a4ba20eb5761331cae0a38812b278e1a8d
Christopher <cn@wtf-solutions.de>
2017-07-29 15:11:37 +0200

    ➕Added nightmare as headless browser

--- src/main.js
+++ src/main.js
@@ -1,1 +1,18 @@
-console.log('start here');
+require('dotenv').config({path: './environment/.env'}) // call by process.env
+import Nightmare from 'nightmare';
+
+const env = process.env;
+let url = `${env.BASE_URL}.${env.TLD}${env.URL_PATH}${env.USER_KEY}`;
+let nightmare = Nightmare(
+  { show: false } // true = Debug
+);
+
+nightmare
+  .goto(url)
+  .end()
+  .then((result) => {
+    console.log(result);
+  })
+  .catch((error) => {
+    console.error('ERROR', error);
+  });
